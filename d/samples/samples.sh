1680eaf2f3a637355366ed4405a21794d8de4312
michael <michael@michael-laptop>
2010-02-07 13:14:36 -0500

    Edited samples.sh

--- d/samples/samples.sh
+++ d/samples/samples.sh
@@ -2,9 +2,12 @@
 #Things to add:
 #-Check to make sure you can use command gdc
 #-More error messages
-echo -n "Compiling..."
-/opt/gdc/bin/gdc hello.d -o hello
-echo "Done!"
-echo -e "Hello World output:\n"
-./hello
-echo -e "\nFinished."
+
+ls -1 *.d | while read prog
+do	
+	echo -n "Compiling ${prog}..."
+   /opt/gdc/bin/gdc ${prog} -o ${prog}.out
+   echo "Done!"
+   echo -e "${prog} output:\n"
+   ./${prog}.out
+done
