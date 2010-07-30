4d563d825ad278c9f3cab2f92083d087a5121482
michaelp <baseball.mjp@gmail.com>
2010-07-29 23:15:03 -0400

    Fixed accidentally reapplied Windows patch

--- d/phobos2/std/c/stdio.d
+++ d/phobos2/std/c/stdio.d
@@ -126,8 +126,7 @@ version (Win32)
 {
     version (GNU) {
 	// _NFILE is not defined anywhere
-	extern export FILE _imp___iob[5];
-	alias _imp___iob _iob;
+	extern export FILE _iob[5];
     } else {
 	extern FILE _iob[_NFILE];
 	extern void function() _fcloseallp;
