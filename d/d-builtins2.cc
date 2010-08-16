f6412f97605cd633745f1f4f7143454f8ea4bed4
Iain Buclaw <ibuclaw@ubuntu.com>
2010-08-16 09:39:40 +0100

    Added GPL onto files missing it, attributed modifications.

--- d/d-builtins2.cc
+++ d/d-builtins2.cc
@@ -1,6 +1,9 @@
 /* GDC -- D front-end for GCC
    Copyright (C) 2004 David Friedman
    
+   Modified by
+    Michael Parrott, Iain Buclaw, (C) 2010
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
