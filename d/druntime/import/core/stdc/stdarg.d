e3955a6dea0a5a375ef7e27475099ec34526cd42
Iain Buclaw <ibuclaw@ubuntu.com>
2010-10-14 14:15:30 +0100

    Patched core.stdc.stdarg to work with GDC compiler.

--- d/druntime/import/core/stdc/stdarg.d
+++ d/druntime/import/core/stdc/stdarg.d
@@ -6,35 +6,48 @@
  * Authors:   Hauke Duden, Walter Bright
  * Standards: ISO/IEC 9899:1999 (E)
  */
+
+/* NOTE: This file has been patched from the original DMD distribution to
+   work with the GDC compiler.
+ */
 module core.stdc.stdarg;
 
-
-alias void* va_list;
-
-template va_start( T )
+version (GNU)
 {
-    void va_start( out va_list ap, inout T parmn )
+    private import gcc.builtins;
+    alias __builtin_va_list va_list;
+    alias __builtin_va_end va_end;
+    alias __builtin_va_copy va_copy;
+}
+else
+{
+    alias void* va_list;
+    
+    void va_end( va_list ap )
     {
-        ap = cast(va_list) ( cast(void*) &parmn + ( ( T.sizeof + int.sizeof - 1 ) & ~( int.sizeof - 1 ) ) );
+
+    }
+
+    void va_copy( out va_list dest, va_list src )
+    {
+	dest = src;
     }
 }
 
-template va_arg( T )
+// The va_start and va_arg template functions are magically
+// handled by the compiler.
+template va_start(T)
 {
-    T va_arg( inout va_list ap )
+    void va_start(out va_list ap, inout T parmn)
     {
-        T arg = *cast(T*) ap;
-        ap = cast(va_list) ( cast(void*) ap + ( ( T.sizeof + int.sizeof - 1 ) & ~( int.sizeof - 1 ) ) );
-        return arg;
     }
 }
-
-void va_end( va_list ap )
+    
+template va_arg(T)
 {
-
-}
-
-void va_copy( out va_list dest, va_list src )
-{
-    dest = src;
+    T va_arg(inout va_list ap)
+    {
+	T t;
+	return t;
+    }
 }
