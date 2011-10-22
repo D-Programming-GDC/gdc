6a3fd226f4a96a64e45fd4c11302f93b30aae7a7
Iain Buclaw <ibuclaw@ubuntu.com>
2011-10-22 14:43:55 +0100

    build_assign_math_op: Stabilize LHS expression.

--- d/d-glue.cc
+++ d/d-glue.cc
@@ -326,12 +326,21 @@ build_assign_math_op(TOK op, Type * type, Expression * e1, Expression * e2, IRSt
     tree lhs_assign = irs->toElemLvalue(e1b);
     lhs_assign = stabilize_reference(lhs_assign);
 
+
+    tree lhs_var = irs->exprVar(type->toCtype());
+    DECL_INITIAL(lhs_var) = lhs_assign;
+
     tree rhs_assign = build_math_op(out_code, e1->type->toCtype(),
-                                    irs->convertTo(lhs_assign, e1b->type, e1->type), e1->type,
+                                    irs->convertTo(lhs_var, e1b->type, e1->type), e1->type,
                                     e2->toElem(irs), e2->type, irs);
 
-    return build2(MODIFY_EXPR, type->toCtype(), lhs_assign,
-                  irs->convertForAssignment(rhs_assign, e1->type, type));
+    rhs_assign = build2(MODIFY_EXPR, type->toCtype(), lhs_var,
+                        irs->convertForAssignment(rhs_assign, e1->type, type));
+
+    lhs_assign = build2(MODIFY_EXPR, type->toCtype(), lhs_assign, lhs_var);
+
+    lhs_assign = irs->compound(rhs_assign, lhs_assign);
+    return irs->binding(lhs_var, lhs_assign);
 }
 
 elem *
@@ -2651,7 +2660,7 @@ StructLiteralExp::toElem(IRState *irs)
                     if ((sd = needsPostblit(fld_type)) != NULL)
                     {   // Generate _d_arrayctor(ti, from = exp, to = exp_tree)
                         Type * ti = fld_type->nextOf();
-                        exp_tree = irs->localVar(exp_type->toCtype());
+                        exp_tree = irs->localVar(exp_type);
                         tree args[3] = {
                             irs->typeinfoReference(ti),
                             irs->toDArray(exp),
