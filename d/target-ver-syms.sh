94937c447deec679c156cb5618666bbe6b3114b6
Johannes Pfau <johannespfau@gmail.com>
2012-02-09 18:39:17 +0100

    Add Android version definition

--- d/target-ver-syms.sh
+++ d/target-ver-syms.sh
@@ -32,7 +32,12 @@ elf*) ;;
 freebsd*) d_os_versym=FreeBSD ; d_unix=1 ;;
 k*bsd*-gnu) d_os_versym=FreeBSD ; d_unix=1 ;;
 kopensolaris*-gnu) d_os_versym=Solaris; d_unix=1 ;;
-linux*) d_os_versym=linux ; d_unix=1 ;; # This is supposed to be "linux", not "Linux", according to the spec
+linux*) # This is supposed to be "linux", not "Linux", according to the spec
+        d_os_versym=linux ; d_unix=1 ;
+        case "$d_target_os" in
+            *androideabi) d_os_versym=Android; d_os_versym2=linux ;;
+        esac
+        ;;
 mingw32*) case "$target_vendor" in
               pc*) d_os_versym=Win32; d_os_versym2=MinGW32; d_windows=1 ;;
               w64*) d_os_versym=Win64; d_os_versym2=MinGW64; d_windows=1 ;;
@@ -102,5 +107,8 @@ OpenBSD) echo "#define TARGET_OPENBSD 1" ;;
 Solaris) echo "#define TARGET_SOLARIS 1" ;;
 Win32)   echo "#define TARGET_WINDOS  1" ;;
 Win64)   echo "#define TARGET_WINDOS  1" ;;
+Android) echo "#define TARGET_ANDROID 1";
+         echo "#define TARGET_LINUX 1";
+         ;;
 *)       echo "#define TARGET_UNIX    1" ;;
 esac
