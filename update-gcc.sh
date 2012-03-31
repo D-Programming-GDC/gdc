f15a288663b7f239e86fc8081edc370c945ec7cd
Iain Buclaw <ibuclaw@ubuntu.com>
2012-03-31 08:55:53 +0100

    Add simple copy script to add gdc to gcc sources.

---
+++ update-gcc.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# -1. Make sure we know where the top-level GCC source directory is
+if test -d gcc && test -d gcc/d && test -f gcc/d/setup-gcc.sh; then
+    if test $# -gt 0; then
+        :
+    else
+        echo "Usage: $0 [OPTION] PATH"
+        exit 1
+    fi
+else
+    echo "This script must be run from the top-level D source directory."
+    exit 1
+fi
+
+d_gccsrc=
+d_setup_gcc=0
+top=`pwd`
+
+# Read command line arguments
+for arg in "$@"; do
+    case "$arg" in
+        --setup) d_setup_gcc=1 ;;
+        *)
+            if test -z "$d_gccsrc" && test -d "$arg" && test -d "$arg/gcc"; then
+                d_gccsrc="$arg";
+            else
+                echo "error: invalid option '$arg'"
+                exit 1
+            fi ;;
+    esac
+done
+
+
+# 0. Remove d sources from d_gccsrc if already exist
+test -h "$d_gccsrc/gcc/d" && rm "$d_gccsrc/gcc/d"
+test -d "$d_gccsrc/libphobos" && rm -r "$d_gccsrc/libphobos"
+if test -e "$d_gccsrc/gcc/d" -o -e "$d_gccsrc/libphobos"; then
+    echo "error: cannot update gcc source, please remove D sources by hand."
+    exit 1
+fi
+
+
+# 1. Copy sources
+ln -s "$top/gcc/d" "$d_gccsrc/gcc/d" && \
+  mkdir "$d_gccsrc/libphobos"        && \
+  cd "$d_gccsrc/libphobos"           && \
+  ../symlink-tree "$top/libphobos"   && \
+  cd $top
+
+
+# 2. Maybe run setup-gcc.sh
+if test $d_setup_gcc -eq 1; then
+    cd $d_gccsrc             && \
+        ./gcc/d/setup-gcc.sh && \
+        cd $top
+fi
+
+echo "GDC update complete."
+exit 0
