495a4989ba2a5f4accb541b475e34975ad380727
ibuclaw <ibuclaw@ubuntu.com>
2013-02-14 09:45:17 +0000

    Remove D_DECL_IS_CONTRACT macro.

--- gcc/d/d-decls.cc
+++ gcc/d/d-decls.cc
@@ -533,7 +533,6 @@ FuncDeclaration::toSymbol (void)
 	  // So are ensure and require contracts.
 	  if (ident == Id::ensure || ident == Id::require)
 	    {
-	      D_DECL_IS_CONTRACT (fndecl) = 1;
 	      DECL_ARTIFICIAL (fndecl) = 1;
 	      TREE_PUBLIC (fndecl) = 1;
 	    }
