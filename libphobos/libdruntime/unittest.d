ae718b1dc0bd9c6e01880b52a429594113a36401
ibuclaw <ibuclaw@ubuntu.com>
2013-05-17 23:12:47 +0100

    Add unittest hook into libdruntime.

---
+++ libphobos/libdruntime/unittest.d
@@ -0,0 +1,38 @@
+// Written in the D programming language.
+
+/**
+ * This test program pulls in all the library modules in order to run the unit
+ * tests on them.
+ */
+
+public import core.sys.posix.sys.select;
+public import core.demangle;
+public import core.time;
+public import core.bitop;
+public import core.thread;
+public import core.exception;
+public import core.math;
+public import etc.linux.memoryerror;
+public import gc.gcbits;
+public import rt.adi;
+public import rt.arrayshort;
+public import rt.arrayfloat;
+public import rt.arrayint;
+public import rt.typeinfo.ti_Aint;
+public import rt.switch_;
+public import rt.arrayreal;
+public import rt.arraybyte;
+public import rt.aaA;
+public import rt.qsort;
+public import rt.lifetime;
+public import rt.minfo;
+public import rt.arraydouble;
+public import rt.util.hash;
+public import rt.util.container;
+public import rt.util.utf;
+public import rt.aApplyR;
+public import rt.arraycast;
+
+void main()
+{
+}
