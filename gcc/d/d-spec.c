4e514d924f2b0f478aa765551591113587338e58
ibuclaw <ibuclaw@ubuntu.com>
2013-08-29 16:32:25 +0100

    Remove dead code from driver.

--- gcc/d/d-spec.c
+++ gcc/d/d-spec.c
@@ -512,16 +512,13 @@ lang_specific_driver (struct cl_decoded_option **in_decoded_options,
 }
 
 /* Called before linking.  Returns 0 on success and -1 on failure.  */
+
 int lang_specific_pre_link (void)  /* Not used for D.  */
 {
   return 0;
 }
 
 /* Number of extra output files that lang_specific_pre_link may generate.  */
+
 int lang_specific_extra_outfiles = 0;  /* Not used for D.  */
 
-/* Table of language-specific spec functions.  */
-const struct spec_function lang_specific_spec_functions[] =
-{
-    { 0, 0 }  /* Not used for D.  */
-};
