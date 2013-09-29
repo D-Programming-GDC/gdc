eb775290a74e9c7ff6897a60ce253a45fbda1642
Iain Buclaw <ibuclaw@ubuntu.com>
2013-09-29 15:14:52 +0100

    Update copyright years.

--- gcc/d/d-objfile.cc
+++ gcc/d/d-objfile.cc
@@ -1,5 +1,5 @@
 // d-objfile.cc -- D frontend for GCC.
-// Copyright (C) 2011, 2012 Free Software Foundation, Inc.
+// Copyright (C) 2011-2013 Free Software Foundation, Inc.
 
 // GCC is free software; you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free
