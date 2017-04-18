63bde4e55fd3c5b58fdee5ee867b5d9538fc21f0
Iain Buclaw <ibuclaw@gdcproject.org>
2017-04-19 00:22:29 +0200

    Remove cc1d spec from lang-spec.h

--- gcc/d/lang-specs.h
+++ gcc/d/lang-specs.h
@@ -25,7 +25,7 @@ along with GCC; see the file COPYING3.  If not see
 {".di", "@d", 0, 1, 0 },
 {".DI", "@d", 0, 1, 0 },
 {"@d",
-  "%{!E:cc1d %i %(cc1_options) %(cc1d) %I %{nostdinc*} %{I*} %{J*} \
+  "%{!E:cc1d %i %(cc1_options) %I %{nostdinc*} %{I*} %{J*} \
     %{MD:-MD %b.deps} %{MMD:-MMD %b.deps} \
     %{M} %{MM} %{MF*} %{MG} %{MP} %{MQ*} %{MT*} \
     %{X:-Xf %b.json} %{Xf*} \
