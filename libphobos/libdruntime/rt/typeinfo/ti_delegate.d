462744b4428b8084ef0a9120c24d52e587d734e4
Iain Buclaw <ibuclaw@gdcproject.org>
2018-06-22 01:01:18 +0200

    Update druntime object module to dmd/stable

--- libphobos/libdruntime/rt/typeinfo/ti_delegate.d
+++ libphobos/libdruntime/rt/typeinfo/ti_delegate.d
@@ -2,7 +2,7 @@
  * TypeInfo support code.
  *
  * Copyright: Copyright Digital Mars 2004 - 2009.
- * License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * License:   $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
  * Authors:   Walter Bright
  */
 
@@ -26,7 +26,7 @@ class TypeInfo_D : TypeInfo
     pure:
     nothrow:
 
-    override size_t getHash(in void* p)
+    override size_t getHash(scope const void* p)
     {
         return rt.util.hash.hashOf(p[0 .. dg.sizeof], 0);
     }
