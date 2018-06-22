9f7ebfbd5de7b950368bb2840392d6c69f1fcfa6
Iain Buclaw <ibuclaw@gdcproject.org>
2018-06-23 00:29:27 +0200

    Update druntime bindings to dmd/stable

--- libphobos/libdruntime/core/sys/posix/grp.d
+++ libphobos/libdruntime/core/sys/posix/grp.d
@@ -2,7 +2,7 @@
  * D header file for POSIX.
  *
  * Copyright: Copyright Sean Kelly 2005 - 2009, Sönke Ludwig 2013.
- * License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+ * License:   $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
  * Authors:   Sean Kelly, Alex Rønne Petersen, Sönke Ludwig
  * Standards: The Open Group Base Specifications Issue 6, IEEE Std 1003.1, 2004 Edition
  */
@@ -97,6 +97,16 @@ else version( OpenBSD )
         char**  gr_mem;
     }
 }
+else version( DragonFlyBSD )
+{
+    struct group
+    {
+        char*   gr_name;
+        char*   gr_passwd;
+        gid_t   gr_gid;
+        char**  gr_mem;
+    }
+}
 else version( Solaris )
 {
     struct group
@@ -108,6 +118,26 @@ else version( Solaris )
     }
 }
 else version( CRuntime_Bionic )
+{
+    struct group
+    {
+        char*   gr_name;
+        char*   gr_passwd;
+        gid_t   gr_gid;
+        char**  gr_mem;
+    }
+}
+else version( CRuntime_UClibc )
+{
+    struct group
+    {
+        char*   gr_name;
+        char*   gr_passwd;
+        gid_t   gr_gid;
+        char**  gr_mem;
+    }
+}
+else version( CRuntime_Musl )
 {
     struct group
     {
@@ -158,6 +188,11 @@ else version( OpenBSD )
     int getgrnam_r(in char*, group*, char*, size_t, group**);
     int getgrgid_r(gid_t, group*, char*, size_t, group**);
 }
+else version( DragonFlyBSD )
+{
+    int getgrnam_r(in char*, group*, char*, size_t, group**);
+    int getgrgid_r(gid_t, group*, char*, size_t, group**);
+}
 else version( Solaris )
 {
     int getgrnam_r(in char*, group*, char*, int, group**);
@@ -165,6 +200,16 @@ else version( Solaris )
 }
 else version( CRuntime_Bionic )
 {
+}
+else version( CRuntime_UClibc )
+{
+    int getgrnam_r(in char*, group*, char*, size_t, group**);
+    int getgrgid_r(gid_t, group*, char*, size_t, group**);
+}
+else version( CRuntime_Musl )
+{
+    int getgrnam_r(in char*, group*, char*, size_t, group**);
+    int getgrgid_r(gid_t, group*, char*, size_t, group**);
 }
 else
 {
@@ -210,6 +255,12 @@ else version( OpenBSD )
     @trusted void endgrent();
     @trusted void setgrent();
 }
+else version( DragonFlyBSD )
+{
+    group* getgrent();
+    @trusted void endgrent();
+    @trusted void setgrent();
+}
 else version( Solaris )
 {
     group* getgrent();
@@ -218,6 +269,18 @@ else version( Solaris )
 }
 else version( CRuntime_Bionic )
 {
+}
+else version( CRuntime_UClibc )
+{
+    group* getgrent();
+    @trusted void endgrent();
+    @trusted void setgrent();
+}
+else version( CRuntime_Musl )
+{
+    group* getgrent();
+    @trusted void endgrent();
+    @trusted void setgrent();
 }
 else
 {
