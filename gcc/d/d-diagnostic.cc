9f584ed630bddaaad44ff36b44e3cd7b6e03c1aa
Iain Buclaw <ibuclaw@gdcproject.org>
2018-10-18 23:53:06 +0200

    Rename expand_format, add more documentation

--- gcc/d/d-diagnostic.cc
+++ gcc/d/d-diagnostic.cc
@@ -30,10 +30,18 @@ along with GCC; see the file COPYING3.  If not see
 
 
 /* Rewrite the format string FORMAT to deal with any format extensions not
-   supported by pp_format().  The result should be freed by the caller.  */
+   supported by pp_format().
+
+   The following format specifiers are handled:
+   `...`: text within backticks gets quoted as '%<...%>'.
+   %-10s: left-justify format flag is removed leaving '%s' remaining.
+   %02x: zero-padding format flag is removed leaving '%x' remaining.
+   %X: uppercase unsigned hexadecimals are rewritten as '%x'.
+
+   The result should be freed by the caller.  */
 
 static char *
-expand_format (const char *format)
+expand_d_format (const char *format)
 {
   OutBuffer buf;
   bool inbacktick = false;
@@ -120,7 +128,7 @@ d_diagnostic_report_diagnostic (const Loc& loc, int opt, const char *format,
     {
       rich_location rich_loc (line_table, make_location_t (loc));
       diagnostic_info diagnostic;
-      char *xformat = expand_format (format);
+      char *xformat = expand_d_format (format);
 
       diagnostic_set_info (&diagnostic, xformat, &argp, &rich_loc, kind);
       if (opt != 0)
@@ -135,7 +143,7 @@ d_diagnostic_report_diagnostic (const Loc& loc, int opt, const char *format,
       text_info text;
       text.err_no = errno;
       text.args_ptr = &argp;
-      text.format_spec = expand_format (format);
+      text.format_spec = expand_d_format (format);
       text.x_data = NULL;
 
       pp_format_verbatim (global_dc->printer, &text);
@@ -145,8 +153,9 @@ d_diagnostic_report_diagnostic (const Loc& loc, int opt, const char *format,
   va_end (argp);
 }
 
-/* Print a hard error message with explicit location LOC, increasing the
-   global or gagged error count.  */
+/* Print a hard error message with explicit location LOC with an optional
+   message prefix PREFIX1 and PREFIX2, increasing the global or gagged
+   error count.  */
 
 void ATTRIBUTE_GCC_DIAG(2,3)
 error (const Loc& loc, const char *format, ...)
@@ -159,17 +168,17 @@ error (const Loc& loc, const char *format, ...)
 
 void ATTRIBUTE_GCC_DIAG(2,0)
 verror (const Loc& loc, const char *format, va_list ap,
-	const char *p1, const char *p2, const char *)
+	const char *prefix1, const char *prefix2, const char *)
 {
   if (!global.gag || global.params.showGaggedErrors)
     {
       char *xformat;
 
       /* Build string and emit.  */
-      if (p2 != NULL)
-	xformat = xasprintf ("%s %s %s", p1, p2, format);
-      else if (p1 != NULL)
-	xformat = xasprintf ("%s %s", p1, format);
+      if (prefix2 != NULL)
+	xformat = xasprintf ("%s %s %s", prefix1, prefix2, format);
+      else if (prefix1 != NULL)
+	xformat = xasprintf ("%s %s", prefix1, format);
       else
 	xformat = xasprintf ("%s", format);
 
@@ -252,8 +261,9 @@ vwarningSupplemental (const Loc& loc, const char *format, va_list ap)
   d_diagnostic_report_diagnostic (loc, 0, format, ap, DK_NOTE, false);
 }
 
-/* Print a deprecation message with explicit location LOC, increasing the
-   global warning or error count depending on how deprecations are treated.  */
+/* Print a deprecation message with explicit location LOC with an optional
+   message prefix PREFIX1 and PREFIX2, increasing the global warning or
+   error count depending on how deprecations are treated.  */
 
 void ATTRIBUTE_GCC_DIAG(2,3)
 deprecation (const Loc& loc, const char *format, ...)
@@ -266,19 +276,19 @@ deprecation (const Loc& loc, const char *format, ...)
 
 void ATTRIBUTE_GCC_DIAG(2,0)
 vdeprecation (const Loc& loc, const char *format, va_list ap,
-	      const char *p1, const char *p2)
+	      const char *prefix1, const char *prefix2)
 {
   if (global.params.useDeprecated == 0)
-    verror (loc, format, ap, p1, p2);
+    verror (loc, format, ap, prefix1, prefix2);
   else if (global.params.useDeprecated == 2 && !global.gag)
     {
       char *xformat;
 
       /* Build string and emit.  */
-      if (p2 != NULL)
-	xformat = xasprintf ("%s %s %s", p1, p2, format);
-      else if (p1 != NULL)
-	xformat = xasprintf ("%s %s", p1, format);
+      if (prefix2 != NULL)
+	xformat = xasprintf ("%s %s %s", prefix1, prefix2, format);
+      else if (prefix1 != NULL)
+	xformat = xasprintf ("%s %s", prefix1, format);
       else
 	xformat = xasprintf ("%s", format);
 
