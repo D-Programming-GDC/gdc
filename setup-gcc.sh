64f8ebb914e2f9f254a4d0d762e2fbb2b5abe8a2
Iain Buclaw <ibuclaw@gdcproject.org>
2018-12-23 20:52:23 +0100

    Update gcc patches

--- setup-gcc.sh
+++ setup-gcc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-d_gdcsrc=$(realpath $(dirname $0))
-d_gccsrc=
+d_gdcsrc=$(realpath $(dirname $0))/gdc
+d_gccsrc=gcc
 d_update_gcc=0
 d_quilt_patch=0
 top=$(pwd)
@@ -22,45 +22,11 @@ for arg in "$@"; do
         --quilt) d_quilt_patch=1 ;;
         --update) d_update_gcc=1 ;;
         *)
-            if test -z "$d_gccsrc" && test -d "$arg" && test -d "$arg/gcc"; then
-                d_gccsrc="$arg";
-            else
-                echo "error: '$arg' is not a valid option or path"
-                exit 1
-            fi ;;
+            echo "error: '$arg' is not a valid option"
+            exit 1 ;;
     esac
 done
 
-
-# 0. Find out what GCC version this is
-if grep version_string $d_gccsrc/gcc/version.c | grep -q '"3.4'; then
-    gcc_ver=3.4
-elif grep version_string $d_gccsrc/gcc/version.c | grep -q '"4.0'; then
-    gcc_ver=4.0
-elif grep -q -E '^4\.[1-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=$(grep -oh -E '^4\.[0-9]+|$' $d_gccsrc/gcc/BASE-VER)
-elif grep -q -E '^5\.[0-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=5
-elif grep -q -E '^6\.[0-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=6
-elif grep -q -E '^7\.[0-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=7
-elif grep -q -E '^8\.[0-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=8
-elif grep -q -E '^9\.[0-9]+([^0-9]|$)' $d_gccsrc/gcc/BASE-VER; then
-    gcc_ver=9
-else
-    echo "cannot get gcc version"
-    exit 1
-fi
-echo "found gcc version $gcc_ver"
-gcc_patch_key=${gcc_ver}.patch
-
-# Determine if this version of GCC is supported
-if test ! -f "$d_gdcsrc/gcc/d/patches/patch-gcc-$gcc_patch_key"; then
-    echo "This version of GCC ($gcc_ver) is not supported."
-    exit 1
-fi
 
 # 1. Clean the GCC tree if any changes were already made to it.
 
@@ -78,7 +44,7 @@ if test $d_quilt_patch -eq 1 && test -d "$d_gccsrc/.pc"; then
 fi
 
 # Remove D sources from d_gccsrc if already exist
-test -h "$d_gccsrc/gcc/d" && rm "$d_gccsrc/gcc/d"
+test -d "$d_gccsrc/gcc/d" && rm -r "$d_gccsrc/gcc/d"
 test -d "$d_gccsrc/libphobos" && rm -r "$d_gccsrc/libphobos"
 if test -e "$d_gccsrc/gcc/d" -o -e "$d_gccsrc/libphobos"; then
     echo "error: cannot update gcc source, please remove D sources by hand."
@@ -91,26 +57,24 @@ test -d "$d_test/gdc.dg" && rm -r "$d_test/gdc.dg"
 test -d "$d_test/gdc.test" && rm -r "$d_test/gdc.test"
 test -e "$d_test/lib/gdc.exp" && rm "$d_test/lib/gdc.exp"
 test -e "$d_test/lib/gdc-dg.exp" && rm "$d_test/lib/gdc-dg.exp"
-if test -e "$d_test/gdc.test" -o -e "$d_test/lib/gdc.exp" -o -e "$d_test/lib/gdc-dg.exp"; then
+if test -e "$d_test/gdc.dg" -o -e "$d_test/gdc.test" -o -e "$d_test/lib/gdc.exp" -o -e "$d_test/lib/gdc-dg.exp"; then
     echo "error: cannot update gcc source, please remove D testsuite sources by hand."
     exit 1
 fi
 
 # 2. Copy sources
-ln -s "$d_gdcsrc/gcc/d" "$d_gccsrc/gcc/d"   && \
-  mkdir "$d_gccsrc/libphobos"               && \
-  cd "$d_gccsrc/libphobos"                  && \
-  ../symlink-tree "$d_gdcsrc/libphobos"     && \
-  cd "../gcc/testsuite"                     && \
-  ../../symlink-tree "$d_gdcsrc/gcc/testsuite" && \
-  cd $top
+cp -av "$d_gdcsrc/gcc/d" "$d_gccsrc/gcc/d" || exit 1
+cp -av "$d_gdcsrc/libphobos" "$d_gccsrc/libphobos" || exit 1
+cp -av "$d_gdcsrc/gcc/testsuite/gdc.dg" "$d_gccsrc/gcc/testsuite/gdc.dg" || exit 1
+cp -av "$d_gdcsrc/gcc/testsuite/gdc.test" "$d_gccsrc/gcc/testsuite/gdc.test" || exit 1
+cp -av "$d_gdcsrc/gcc/testsuite/lib/gdc.exp" "$d_gccsrc/gcc/testsuite/lib/gdc.exp" || exit 1
+cp -av "$d_gdcsrc/gcc/testsuite/lib/gdc-dg.exp" "$d_gccsrc/gcc/testsuite/lib/gdc-dg.exp" || exit 1
 
 
 if test $d_update_gcc -eq 1; then
   echo "GDC update complete"
   exit 0
 fi
-
 
 # 3. Patch the GCC tree, both toplevel and subdirectories.
 #
@@ -126,28 +90,28 @@ fi
 if test $d_quilt_patch -eq 1; then
   cd $d_gccsrc && \
     mkdir -p .pc && \
-    echo "gcc/d/patches" > .pc/.quilt_patches && \
+    echo "../patches" > .pc/.quilt_patches && \
     echo "series" > .pc/.quilt_series || exit 1
 
-  cat > gcc/d/patches/series << EOF
-patch-toplev-${gcc_patch_key}
-patch-toplev-ddmd-${gcc_patch_key}
-patch-gcc-${gcc_patch_key}
-patch-gcc-ddmd-${gcc_patch_key}
-patch-targetdm-${gcc_patch_key}
-EOF
+  if test -f "../patches/gcc.patch"; then
+    echo gcc.patch >> ../patches/series
+  fi
+  if test -f "../patches/ddmd.patch"; then
+    echo ddmd.patch >> ../patches/series
+  fi
 
   quilt upgrade && \
     quilt push -a && \
     cd $top || exit 1
 else
-  cd $d_gccsrc && \
-    patch -p1 -i gcc/d/patches/patch-toplev-${gcc_patch_key} && \
-    patch -p1 -i gcc/d/patches/patch-toplev-ddmd-${gcc_patch_key} && \
-    patch -p1 -i gcc/d/patches/patch-gcc-${gcc_patch_key} && \
-    patch -p1 -i gcc/d/patches/patch-gcc-ddmd-${gcc_patch_key} && \
-    patch -p1 -i gcc/d/patches/patch-targetdm-${gcc_patch_key} && \
-    cd $top || exit 1
+  cd $d_gccsrc
+  if test -f "../patches/gcc.patch"; then
+    patch -p1 -i ../patches/gcc.patch || exit 1
+  fi
+  if test -f "../patches/ddmd.patch"; then
+    patch -p1 -i ../patches/ddmd.patch || exit 1
+  fi
+  cd $top
 fi
 
 echo "GDC setup complete."
